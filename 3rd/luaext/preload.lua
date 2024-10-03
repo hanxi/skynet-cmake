04c1381425b5af8f980fabc5a36d8d375176d632
cloudfreexiao <996442717qqcom@gmail.com>
2024-10-03 12:47:14 +0800

    add splash

--- 3rd/luaext/preload.lua
+++ 3rd/luaext/preload.lua
@@ -1,7 +1,13 @@
-stringx = require("misc.stringx")
-tablex = require("misc.tablex")
+-- https://github.com/davisdude/mlib
+-- https://github.com/rxi/lume
+-- https://github.com/kikito/bump.lua
 
 require("luaoop.class")
 require("luaoop.enum")
 require("luaoop.mixin")
 require("luaoop.property")
+
+stringx = require("misc.stringx")
+tablex = require("misc.tablex")
+
+
